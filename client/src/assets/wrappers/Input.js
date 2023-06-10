154bae90a59015401d4e93948a6ee6a0c64987f7
Vipulp007 <96169016+Vipulp007@users.noreply.github.com>
2023-06-10 10:47:35 +0530

    Delete client/src directory

--- client/src/assets/wrappers/Input.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-const Input = styled.input`
-  width: 100%;
-  color: red;
-  @media (max-width: 515px) {
-    margin-bottom: 10px;
-  }
-`;
-export default Input;
--- client/src/pages/dashboard/index.js
+++
@@ -1,7 +0,0 @@
-import AddJob from './AddJob';
-import AllJobs from './AllJobs';
-import Profile from './Profile';
-import SharedLayout from './SharedLayout';
-import Stats from './Stats';
-import ImageUploads from './ImageUploads';
-export { AllJobs, Profile, SharedLayout, Stats, AddJob, ImageUploads };
